99bed4b63eb3234e92fb92245bb81f871646319e
BcodezMUJJE <billbrightmateg@gmail.com>
2023-10-30 03:21:05 -0700

    gather data

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-
-"""this returns information about his/her TODO list progress"""
-
-import requests
+"""this script using this REST API, for a given employee ID,
+returns information about his/her TODO list progress."""
+import requests as r
 import sys
 
-if _name_ == "_main_":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
-
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
-
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com/'
+    usr_id = r.get(url + 'users/{}'.format(sys.argv[1])).json()
+    to_do = r.get(url + 'todos', params={'userId': sys.argv[1]}).json()
+#    print(to_do)
+    completed = [title.get("title") for title in to_do if
+                 title.get('completed') is True]
+    print(completed)
+    print("Employee {} is done with tasks({}/{}):".format(usr_id.get("name"),
+                                                          len(completed),
+                                                          len(to_do)))
+    [print("\t {}".format(title)) for title in completed]
--- 0x15-api/README.md
+++ 0x15-api/README.md
@@ -1,13 +1,5 @@
 0x15. API
-
 Python
 Scripting
 Back-end
 API
-
-TASKS
-
-0. Gather data from an API
-1. Export to CSV
-2. Export to JSON
-3. Dictionary of list of dictionaries
